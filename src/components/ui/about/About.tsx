d27650cf3ca3069372145efe406eb4194c6f5a23
kaitomizuchi <kaitoshuinei42@gmail.com>
2025-08-30 12:57:45 +0900

    aboutセクション完成

--- src/components/ui/about/About.tsx
+++ src/components/ui/about/About.tsx
@@ -1,17 +1,83 @@
 import React from 'react';
 import s from './about.module.scss';
 import Section from '../section/Section';
+import { Pencil, Star } from 'lucide-react';
+import { skills } from './skils';
 
 const About = () => {
   return (
     <Section title='About'>
       <div className={s.inner}>
         <div className={s.column}>
-          <div className={s.introduction_container}>自己紹介</div>
+          <div className={s.introduction_container}>
+            <h3 className={s.container_title}>自己紹介</h3>
+            <ul className={s.introduction_list}>
+              <li className={s.introduction_item}>
+                <h4 className={s.item_title}>名前</h4>
+                <p className={s.item_text}>
+                  水内 海斗<span className={s.small_text}>みずうち かいと</span>
+                </p>
+              </li>
+              <li className={s.introduction_item}>
+                <h4 className={s.item_title}>年齢</h4>
+                <p className={s.item_text}>24歳 エンジニア歴3年</p>
+              </li>
+              <li className={s.introduction_item}>
+                <h4 className={s.item_title}>出身地</h4>
+                <p className={s.item_text}>東京都八王子市</p>
+              </li>
+              <li className={s.introduction_item}>
+                <h4 className={s.item_title}>心が躍ること</h4>
+                <div className={s.item_text}>
+                  <p className={s.exciting_text}>
+                    <Star size={16} />
+                    知らない土地を目的を決めず散歩すること
+                  </p>
+                  <p className={s.exciting_text}>
+                    <Star size={16} />
+                    寺社仏閣をめぐり建築美や自然を感じること
+                  </p>
+                </div>
+              </li>
+            </ul>
+          </div>
         </div>
         <div className={s.column}>
-          <div className='skill_container'>得意技術</div>
-          <div className={s.certifications_container}>資格</div>
+          <div className={s.skill_container}>
+            <h3 className={s.container_title}>スキル</h3>
+            {skills.map((category, index) => (
+              <div
+                key={category.label}
+                className={`${s.skill_category} ${s[`category_${index + 1}`]}`}
+              >
+                <h4 className={s.category_title}>{category.label}</h4>
+                <ul className={s.skill_list}>
+                  {category.list.map(item => (
+                    <li key={item} className={s.skill_item}>
+                      {item}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            ))}
+          </div>
+          <div className={s.certifications_container}>
+            <h3 className={s.container_title}>資格</h3>
+            <ul className={s.certifications_list}>
+              <li className={s.certifications_item}>
+                <Pencil size={16} />
+                HTML5 プロフェッショナル認定資格 レベル1
+              </li>
+              <li className={s.certifications_item}>
+                <Pencil size={16} />
+                AWS認定 クラウドプラクティショナー
+              </li>
+              <li className={s.certifications_item}>
+                <Pencil size={16} />
+                AWS認定 ソリューションアーキテクト – アソシエイト
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </Section>
---
+++ src/components/ui/about/skils.ts
@@ -0,0 +1,18 @@
+export const skills = [
+  {
+    label: 'プログラミング言語',
+    list: ['TypeScript', 'JavaScript(ES2015)'],
+  },
+  {
+    label: 'フロントエンド',
+    list: ['Next.js', 'React', 'Sass(SCSS)', 'HTML5', 'CSS3'],
+  },
+  {
+    label: 'バックエンド',
+    list: ['Node.js', 'Prisma', 'PostgreSQL', 'MySQL'],
+  },
+  {
+    label: '経験のある技術・ツール',
+    list: ['Material UI (MUI)', 'Docker', 'AWS (SAA資格保有・実務未経験)', 'Figma', 'Git / GitHub'],
+  },
+];
